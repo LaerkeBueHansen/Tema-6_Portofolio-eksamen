e8d1dc547dff22779d891e9d203401e7986e8ecd
Lærke <laha0004@stud.kea.dk>
2023-06-12 11:26:40 +0200

    Portofolio commit

---
+++ Portofolie - Web/JS/portofolio.js
@@ -0,0 +1,32 @@
+    /*BURGERMENU*/
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".topn").forEach(n => n.
+addEventListener("click",()=>{
+    hamburger.classList.remove("active");
+   navMenu.classList.remove("active");
+}))
+     /*BURGERMENU*/
+
+     /*ACCORDION*/
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+    /*ACCORDION*/
